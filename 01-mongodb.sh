501c90170cc1c46c2b9daa928737dc8e4114001e
Seth Krishna <sai.krishna@relayhumancloud.com>
2026-07-07 21:52:54 +0530

    mongo1

---
+++ 01-mongodb.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+LOGS_FOLDER="/var/log/roboshop"
+sudo mkdir -p $LOGS_FOLDER 
+sudo chown _R ec2-user:ec2_user $LOGS_FOLDER
+sudo chmod -R 755 $LOGS_FOLDER 
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+
+USER_ID=$(id -u)
+TIME_STAMP=$(date "+%Y-%m-%d %H:%M:%S")
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USER_ID -ne 0]; then 
+    echo -e "$TIME_STAMP [ERROR] $R Please run this script with root access $N" | tee -a $LOGS_FILE
+    exit 1 
+fi 
+
+VALIDATE(){
+    if [$1 -ne 0 ]; then 
+        echo -e "$TIME_STAMP [ERROR] $2.... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1 
+    else 
+        echo -e "$TIMESTAMP [INFO] $2....$G SUCCESS $N" | tee -a $LOGS_FILE
+    fi 
+
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding Mongo repo"
