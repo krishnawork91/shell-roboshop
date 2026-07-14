5dc089dd50ca6376fce3e5a799e18ab5d467a8c6
Seth Krishna <sai.krishna@relayhumancloud.com>
2026-07-14 13:54:41 +0530

    test

--- 05-catalogue.sh
+++ 05-catalogue.sh
@@ -5,6 +5,7 @@ sudo mkdir -p $LOGS_FOLDER
 sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
 sudo chmod -R 755 $LOGS_FOLDER 
 LOGS_FILE="$LOGS_FOLDER/$0.log"
+SCRIPT_DIR=$PWD
 
 USER_ID=$(id -u)
 TIME_STAMP=$(date "+%Y-%m-%d %H:%M:%S")
@@ -41,5 +42,25 @@ else
     echo "System user roboshop already created...$Y SKIPPING $N" 
 fi 
 
+rm -rf /app 
+VALIDATE $? "removing existing code"
+
+rm -rf /tmp/catalogue.zip
+VALIDATE $? "Remove catalogue zip"
+
 mkdir -p /app &>> LOGS_FILE 
 VALIDATE $? "Creating app directory"  
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>> LOGS_FILE
+cd /app 
+unzip /tmp/catalogue.zip &>> LOGS_FILE 
+VALIDATE $? "Downloaded and extracted catalogue code"
+
+npm install &>> LOGS_FILE 
+VALIDATE $? "Installing dependencies" 
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service 
+VALIDATE $? "Created systemctl service"
+
+cp 
+
